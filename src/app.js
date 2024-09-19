e6825deb049879b439f5fa29db4f40cc330cd79a
prasanthpulavarthi <prasanthpulavarthi19@gmail.com>
2024-09-19 22:26:46 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 const express= require('express');
 const app=express()
+
+
 app.use("/test",(req,res)=>{
     res.send("hello form test")
-console.log("hello")
-})
-app.use((req,res)=>{
-    res.send("hello11")
-console.log("hello")
 })
 
-app.listen(7000)
+app.get("/user",(req,res)=>{
+    res.send({firstName:"sai",lastName:"prasanth"})
+})
+app.post("/user",(req,res)=>{
+    //data saved in database
+    res.send("data saved successfully")
+})
+app.delete("/user",(req,res)=>{
+    //data saved in database
+    res.send("data deleted")
+})
+
+
+
+app.listen(3000)
