74fe6dd6ada861ee1a370f76870b6a5635aec2cc
prasanthpulavarthi <prasanthpulavarthi19@gmail.com>
2024-09-23 22:59:36 +0530

    crud

--- src/app.js
+++ src/app.js
@@ -1,26 +1,57 @@
 const express= require('express');
 const connectDB = require("./utils/database");
-const User=require("./models/user")
+const User=require("./models/user");
 
 const app=express()
 app.use(express.json())
 
 
-app.post("/signup", async(req,res)=>{
-const user= new User(req.body)
+// app.post("/signup", async(req,res)=>{
+// const user= new User(req.body)
 
-try{
-    await user.save();
-    res.send("data posted succssfuly")
-}
-catch(err){
-    res.status(400).send("error saving the user"+err.messsage)
+// try{
+//     await user.save();
+//     res.send("data posted succssfuly")
+// }
+// catch(err){
+//     res.status(400).send("error saving the user"+err.messsage)
 
-}
+// }
 
 
+
+// })
+
+//for delete
+// app.delete("/user", async(req,res)=>{
+//     const userID= req.body.userID
+//      try{
+//         const user=await User.findByIdAndDelete(userID)
+//         res.send("data posted succssfuly")
+//     }
+//     catch(err){
+//         res.status(400).send("error saving the user"+err.messsage)
+    
+//     }
+// })
+
+//update
+app.patch("/user", async(req,res)=>{
+    const id=req.body.userID
+    const data=req.body
+    try{
+    await User.findByIdAndUpdate(id,data)
+        res.send("data changes successfully")
+
+
+    }
+    catch(err){
+            res.status(400).send("error saving the user"+err.messsage)
+        
+         }
 
 })
+
 
 
 
