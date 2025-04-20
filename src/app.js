9ec80fc480565d49e08dd82b2816b30ab402c336
prasanthpulavarthi <prasanthpulavarthi19@gmail.com>
2025-04-20 22:57:02 +0530

    passowrd bcrypt

--- src/app.js
+++ src/app.js
@@ -2,20 +2,24 @@ const express=require("express");
 const {connectDB}=require("./config/database")
 const app=express();
 const User=require("./models/user")
+const {validateSignUpData}=require("./utils/validation")
 app.use(express.json())
 // const {authUser}=require("./middleware/authuser")
+const bcrypt=require("bcryptjs")
 
 app.post("/signup",async(req,res)=>{
-    const user= new User(req.body)
-
-   
     try{
-        user.save()
+        validateSignUpData(req);
+        const {firstName,emailId,password}=req.body;
+        const passwordHash=await bcrypt.hash(password,10)
+      console.log(passwordHash)  
+        const user= new User({firstName,emailId,password:passwordHash});
+        await user.save()
         res.send("data saved successfully")
 
     }
     catch(err){
-        res.status(400).send(err.message)
+        res.status(400).send("Error : "+err.message)
 
     }
   
@@ -23,7 +27,57 @@ app.post("/signup",async(req,res)=>{
 
 }
 
-)
+);
+app.post("/login",async (req,res)=>{
+    try{
+        const {emailId,password}=req.body;
+        const user=await User.findOne({emailId:emailId});
+        if(!user){
+            throw new Error("invalid credentials")
+        }
+
+        const isPasswordValid=await bcrypt.compare(password,user.password);
+        if(isPasswordValid){
+            res.send("user login successful")
+        }
+        else{
+            throw new Error("invalid credentials")
+        }
+
+
+
+    }
+    catch(err){
+        res.status(400).send(err.message)
+    }
+})
+
+
+app.patch("/user/:userId",async(req,res)=>{
+    const userId=req.params?.userId;
+    const data=req.body;
+   
+    try{
+        const ALLOWED_UPDATES=["about","gender","photoUrl","skills"];
+        const isUpdateAllowed=Object.keys(data).every((k)=>
+            ALLOWED_UPDATES.includes(k)
+    );
+        if(!isUpdateAllowed){
+            throw new Error("update not allowed");
+        };
+        if(data?.skills.length>10){
+            throw new Error("skill cannot be more than 10")
+        }
+        const user=await User.findByIdAndUpdate({_id:userId},data,{
+            returnDocument:"after",
+            runValidators:true
+        });
+    }
+    catch(err){
+        res.status(400).send("update error: "+err.message)
+    }
+
+});
 app.get("/user",async (req,res)=>{
     try{
         const user=await User.findOne({age:req.body.age})
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose=require("mongoose");
+const validator=require("validator")
 const userSchema= new mongoose.Schema({
     firstName:{
         type:String,
@@ -14,6 +15,11 @@ const userSchema= new mongoose.Schema({
         lowercase:true,
         unique:true,
         trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("invalid email address" + value)
+            }
+        }
 
     },
     password:{
